a4fdebba0743f4ff18cc3288ffe3478f0de1a1dd
Codacblackmoon <gideonlenge96@gmail.com>
2024-10-05 00:11:09 +0200

    Finshing  about page

--- src/Getstarted.tsx
+++ src/Getstarted.tsx
@@ -4,6 +4,7 @@ import ImageUpload from './components/ImageUpload';
 
 const Getstarted: React.FC = () => {
   return (
+    
     <div className="homepage glassr">
       <h1 id="third_tital" >Image Upload</h1>
       <ImageUpload/>
